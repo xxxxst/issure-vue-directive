7ecebdbaad0bf9690549275cf1ed8e0f7ff54497
chenlinyanfa@superscene.com.cn <chenlinyanfa@superscene.com.cn>
2021-04-02 17:42:10 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+app.directive('test', {
+	beforeMount: function (el, data: any, tag: any) {
+		console.info("beforeMount");
+	},
+	mounted: function (el, data: any, tag: any) {
+		console.info("mounted");
+	},
+	beforeUpdate: function (el, data: any, tag: any) {
+		console.info("beforeUpdate");
+	},
+	updated() {
+		console.info("updated");
+	},
+	beforeUnmount: function (el, data: any, tag: any) {
+		console.info("beforeUnmount");
+	},
+	unmounted: function (el, data: any, tag: any) {
+		console.info("unmounted");
+	}
+})
+
+app.mount('#app')
